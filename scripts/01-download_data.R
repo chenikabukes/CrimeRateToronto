a2d2240d0a3630b18d50471febc642df7aa60045
Chenika Bukes <chenikacatherin@gmail.com>
2024-09-17 20:32:39 -0400

    Add initial download_data script and raw_data csv file.

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -1,26 +1,36 @@
 #### Preamble ####
-# Purpose: Downloads and saves the data from [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Downloads and saves the data from the TTC's Transport Bus Initiative Usage dataset on Open Data Toronto
+# Author: Chenika Bukes
+# Date: 17/09/2024
+# Contact: chenika.bukes@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: 
+#   - Access to the Open Data Toronto API
+#   - The `opendatatoronto` and `tidyverse` R packages installed
+#   - An internet connection to download the data
+# Any other information needed? This script downloads data for the Transport Bus Initiative Usage dataset from Open Data Toronto and saves it as a CSV file for further analysis.
 
 
 #### Workspace setup ####
 library(opendatatoronto)
 library(tidyverse)
-# [...UPDATE THIS...]
 
 #### Download data ####
-# [...ADD CODE HERE TO DOWNLOAD...]
+# Get the package information for the Transport Bus Initiative Usage dataset
+package <- show_package("transport-bus-initiative-usage")  # Update this with the correct package ID
+package
 
+# Get all resources for this package
+resources <- list_package_resources("transport-bus-initiative-usage")  # Update this with the correct package ID
 
+# Identify datastore resources; by default, Toronto Open Data sets datastore resource format to CSV for non-geospatial and GeoJSON for geospatial resources
+datastore_resources <- filter(resources, tolower(format) %in% c('csv', 'geojson'))
+
+# Load the first datastore resource as a sample
+the_raw_data <- filter(datastore_resources, row_number() == 1) %>% get_resource()
+the_raw_data
 
 #### Save data ####
-# [...UPDATE THIS...]
-# change the_raw_data to whatever name you assigned when you downloaded it.
-write_csv(the_raw_data, "inputs/data/raw_data.csv") 
+write_csv(the_raw_data, "./data/raw_data/raw_data.csv") 
 
          
